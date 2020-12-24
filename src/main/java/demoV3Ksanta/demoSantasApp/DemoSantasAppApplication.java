714abe50c47fde9312efa20c6a62caeb95ee8d0f
orloveste <orlovevm@yahoo.com>
2020-12-24 10:46:20 +0200

    Initial commit

---
+++ src/main/java/demoV3Ksanta/demoSantasApp/DemoSantasAppApplication.java
@@ -0,0 +1,13 @@
+package demoV3Ksanta.demoSantasApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSantasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSantasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demoV3Ksanta/demoSantasApp/DemoSantasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package demoV3Ksanta.demoSantasApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSantasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
